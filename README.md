7dfde60a00c9c4982aa0b96b3f8a12e94a8753ac
huangxinping <o0402@outlook.com>
2016-07-25 15:10:56 +0800

    first commit.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # XPFullScreenPopGesture
 This class add full screen gesture for UINavigationController, replace your class with it. It's very easy.
+
+# Carthage
+```
+github "huangxinping/XPFullScreenPopGesture"
+```
+
---
+++ XPFullScreenPopGestureNavigationController/XPFullScreenPopGestureNavigationController.h
@@ -0,0 +1,19 @@
+//
+//  XPFullScreenPopGestureNavigationController.h
+//  XPFullScreenPopGestureNavigationController
+//
+//  Created by xinpinghuang on 16/7/25.
+//  Copyright © 2016年 huangxinping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XPFullScreenPopGestureNavigationController.
+FOUNDATION_EXPORT double XPFullScreenPopGestureNavigationControllerVersionNumber;
+
+//! Project version string for XPFullScreenPopGestureNavigationController.
+FOUNDATION_EXPORT const unsigned char XPFullScreenPopGestureNavigationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XPFullScreenPopGestureNavigationController/PublicHeader.h>
+
+
